6a2f5165b4b3badba21596470c4a11480c4e1cf5
uwevanheesch <uwe@vanheesch.net>
2017-04-26 15:10:55 +0200

    Develop (#2)
    * Added code comments for ebig constants
    
    * Added code comments for ebig constants
    
    * Added code comments for ebig constants
    
    * Updated serial port for use on RaspberryPi

--- myutils/ebig_constants.py
+++ myutils/ebig_constants.py
@@ -1,3 +1,11 @@
+##
+# Constants for Ebig sensor-node to room mapping and sensor types
+#
+# Room locations follow schema: Building-wing-roomNo
+# wing can be: NO (if building has no wings), or A,B,C and the like
+# roomNo follows the room naming scheme of the building, for instance 1.01 for room 1 on the first floor
+#
+##
 ROOM_LOCATIONS = {
     1: '',
     2: '',
@@ -8,13 +16,12 @@ ROOM_LOCATIONS = {
 SENSOR_MAPPING = {
     1: ('TEMP_MIDDLE', 'V_TEMP'),  # Temperature measured in the middle of a room; sub_type=V_TEMP
     2: ('TEMP_WINDOW', 'V_TEMP'),  # Temperature measured on the windows; sub_type=V_TEMP
-    3: ('DOOR_MAIN_OPEN', 'V_TRIPPED'),  # Boolean shows if door to main floor is open; sub_type=V_TRIPPED
-    4: ('DOOR_LEFT_OPEN', 'V_TRIPPED'),  # Boolean shows if door left (seen from main door) is open; sub_type=V_TRIPPED
-    5: ('DOOR_RIGHT_OPEN', 'V_TRIPPED'),
-# Boolean shows if door right (seen from main door) is open; sub_type=V_TRIPPED
-    6: ('WINDOW_ONE_OPEN', 'V_TRIPPED'),  # Boolean shows if window is open; sub_type=V_TRIPPED
-    7: ('WINDOW_TWO_OPEN', 'V_TRIPPED'),  # Boolean shows if window is open; sub_type=V_TRIPPED
-    8: ('WINDOW_THREE_OPEN', 'V_TRIPPED'),  # Boolean shows if window is open; sub_type=V_TRIPPED
+    3: ('DOOR_MAIN_OPEN', 'V_TRIPPED'),  # Shows if door to main floor is open; sub_type=V_TRIPPED
+    4: ('DOOR_LEFT_OPEN', 'V_TRIPPED'),  # Shows if door left (seen from main door) is open; sub_type=V_TRIPPED
+    5: ('DOOR_RIGHT_OPEN', 'V_TRIPPED'),  # Shows if door right (seen from main door) is open; sub_type=V_TRIPPED
+    6: ('WINDOW_ONE_OPEN', 'V_TRIPPED'),  # Shows if window is open; sub_type=V_TRIPPED
+    7: ('WINDOW_TWO_OPEN', 'V_TRIPPED'),  # Shows if window is open; sub_type=V_TRIPPED
+    8: ('WINDOW_THREE_OPEN', 'V_TRIPPED'),  # Shows if window is open; sub_type=V_TRIPPED
     9: ('LIGHT_INTENSITY', 'V_LIGHT_LEVEL'),  # Light-intensity measured in the middle of a room; sub_type=V_LIGHT_LEVEL
 }
 
