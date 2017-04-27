70db63a842cd5c9188cdb2f6d892077fbdbdc700
uwevanheesch <uwe@vanheesch.net>
2017-04-27 10:36:51 +0200

    Added sensor mappings for radiator temperatures (in and out) (#4)

--- myutils/ebig_constants.py
+++ myutils/ebig_constants.py
@@ -1,10 +1,14 @@
 ##
 # Constants for Ebig sensor-node to room mapping and sensor types
 #
+# Measured values as stored in influx measurement "room"
+##
+
+
 # Room locations follow schema: Building-wing-roomNo
 # wing can be: NO (if building has no wings), or A,B,C and the like
 # roomNo follows the room naming scheme of the building, for instance 1.01 for room 1 on the first floor
-#
+# room locations are used as tags in Influx measurement
 ##
 ROOM_LOCATIONS = {
     1: '',
@@ -13,16 +17,19 @@ ROOM_LOCATIONS = {
     105: 'Uwe-NO-0.02'  # Location-ID for Uwe's ground floor
 }
 
+# Sensor ID mapped against field name in Influx, and mysensors sub-type
 SENSOR_MAPPING = {
-    1: ('TEMP_MIDDLE', 'V_TEMP'),  # Temperature measured in the middle of a room; sub_type=V_TEMP
-    2: ('TEMP_WINDOW', 'V_TEMP'),  # Temperature measured on the windows; sub_type=V_TEMP
-    3: ('DOOR_MAIN_OPEN', 'V_TRIPPED'),  # Shows if door to main floor is open; sub_type=V_TRIPPED
-    4: ('DOOR_LEFT_OPEN', 'V_TRIPPED'),  # Shows if door left (seen from main door) is open; sub_type=V_TRIPPED
-    5: ('DOOR_RIGHT_OPEN', 'V_TRIPPED'),  # Shows if door right (seen from main door) is open; sub_type=V_TRIPPED
-    6: ('WINDOW_ONE_OPEN', 'V_TRIPPED'),  # Shows if window is open; sub_type=V_TRIPPED
-    7: ('WINDOW_TWO_OPEN', 'V_TRIPPED'),  # Shows if window is open; sub_type=V_TRIPPED
-    8: ('WINDOW_THREE_OPEN', 'V_TRIPPED'),  # Shows if window is open; sub_type=V_TRIPPED
-    9: ('LIGHT_INTENSITY', 'V_LIGHT_LEVEL'),  # Light-intensity measured in the middle of a room; sub_type=V_LIGHT_LEVEL
+    1: ('TEMP_MIDDLE', 'V_TEMP'),  # Temperature measured in the middle of a room
+    2: ('TEMP_WINDOW', 'V_TEMP'),  # Temperature measured on the windows
+    3: ('DOOR_MAIN_OPEN', 'V_TRIPPED'),  # Shows if door to main floor is open
+    4: ('DOOR_LEFT_OPEN', 'V_TRIPPED'),  # Shows if door left (seen from main door) is open
+    5: ('DOOR_RIGHT_OPEN', 'V_TRIPPED'),  # Shows if door right (seen from main door) is open
+    6: ('WINDOW_ONE_OPEN', 'V_TRIPPED'),  # Shows if window is open
+    7: ('WINDOW_TWO_OPEN', 'V_TRIPPED'),  # Shows if window is open
+    8: ('WINDOW_THREE_OPEN', 'V_TRIPPED'),  # Shows if window is open
+    9: ('LIGHT_INTENSITY', 'V_LIGHT_LEVEL'),  # Light-intensity measured in the middle of a room
+    10: ('TEMP_RAD_IN', 'V_TEMP'),  # Temperature measured on the input pipe of a radiator
+    11: ('TEMP_RAD_OUT', 'V_TEMP'),  # Temperature measured on the output pipe of a radiator
 }
 
 
