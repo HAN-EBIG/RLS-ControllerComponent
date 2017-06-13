d2232ed2496bca4974e6977028153cd4e91af1db
uwevanheesch <uwe@vanheesch.net>
2017-06-13 10:55:34 +0200

    Develop (#10)
    * Added code comments for ebig constants
    
    * Added code comments for ebig constants
    
    * Added code comments for ebig constants
    
    * Updated serial port for use on RaspberryPi
    
    * Updated serial port for use on RaspberryPi
    
    * Updated serial port for use on RaspberryPi
    
    * Added sensor mappings for radiator temperatures (in and out) (#4)
    
    * Added username and password for influx.
    
    * Added constant for humidity
    
    * Added location constants for Jorn's, and Ties' home and a room in the Technovium
    
    * Added constants for power measurement
    
    * Battery Level processing
    
    * Added support for battery level
    
    * Corrected type of battery level sensor message, was float, must be int (percent value)

--- ebig-controller.py
+++ ebig-controller.py
@@ -10,19 +10,19 @@ from myutils.listener import serial_listen
 
 def influx_handler(message):
     print("Message: %s" % message)
+
     if (location_exists(message.node_id) and sensor_exists(message.child_sensor_id)):
-
-        if (message.sub_type != get_expected_subtype(message.child_sensor_id)):
-            return
-
         series = []
-
         base_datapackage = {
             'measurement': 'room',
             'tags': {
-                'roomLocation': decode_room_location(message.node_id)
+                'roomLocation': decode_room_location(message.node_id),
+                'nodeId': message.node_id
             }
         }
+
+        if (message.sub_type != get_expected_subtype(message.child_sensor_id)):
+            return
 
         ebig_sensor = decode_sensor(message.child_sensor_id)
 
@@ -43,7 +43,10 @@ def influx_handler(message):
 
         elif (message.sub_type == 'V_KWH'):
             base_datapackage['fields'] = {ebig_sensor: float(message.payload)}
-            
+
+        elif (message.sub_type == 'I_BATTERY_LEVEL'):
+            base_datapackage['fields'] = {ebig_sensor: int(message.payload)}
+
         series.append(base_datapackage)
         client = InfluxDBClient('145.74.104.50', 8086, 'sensorcontroller', '@sensorpass@', 'ebig')
         client.write_points(series)
